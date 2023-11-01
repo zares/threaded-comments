937ae2ac8eab74d3e17de6407b6431c02c796f6a
zares <sergiozares@gmail.com>
2023-11-01 18:46:29 +0200

    Add Loading... inside form section.

--- resources/views/thread/index.blade.php
+++ resources/views/thread/index.blade.php
@@ -1,4 +1,6 @@
-<div class="js-form-section" data-src="{{ route('comment.form') }}"></div>
+<div class="js-form-section" data-src="{{ route('comment.form') }}">
+    Loading...
+</div>
 <div class="js-thread-list mt-5 md:mt-10">
     @include('thread.items', ['comments' => $comments, 'parent' => null])
 </div>
